6e80181cbfe50d5afd270b2dde9eb83aa1c6234d
JunYoungJee <rainmaker8492@gmail.com>
2022-01-18 19:47:00 +0900

    test

---
+++ StarbucksAutomator/Applicaton.swift
@@ -0,0 +1,13 @@
+//
+//  Applicaton.swift
+//  StarbucksAutomator
+//
+//  Created by sokol on 2022/01/18.
+//
+
+import Cocoa
+
+//Main.storyboard에 있는 애플리케이션 객체를 어떻게 커스터마이징해서 사용하지...
+class Applicaton: NSApplication {
+
+}
